24c2d6dc8d65526802f0acd143fcb354c76d4834
Ian Brault <ian.brault@engineering.ucla.edu>
2018-08-21 21:30:44 -0700

    Better logging messages.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require("express");
 var app = express();
 
+
 // view engine setup
 app.set("views", "views");
 app.set("view engine", "jade");
@@ -14,18 +15,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
+
+// logging
+
+const logfmt = () => {
+    let dateObj = new Date();
+    let date = `${dateObj.getFullYear()}.${dateObj.getMonth()+1}.${dateObj.getDate()}`;
+    let hour = dateObj.getHours().toString().padStart(2, "0");
+    let mins = dateObj.getMinutes().toString().padStart(2, "0");
+    let secs = dateObj.getSeconds().toString().padStart(2, "0");
+    return `[${hour}:${mins}:${secs} ${date}]: `;
+};
+
+const log = (msg) => {
+    console.log(`${logfmt() + msg}`);
+};
+
+const log_error = (emsg) => {
+    console.log(`${logfmt() + emsg}`);
+};
+
+
 // routing
+
 app.get("/", (req, res) => {
-    console.log("GET /");
+    log("GET /");
     res.render("index");
 });
 
+
 // error handler
+
 app.use((err, req, res, next) => {
     let emsg = `Error [${err.status || 500}]: ${err.message}`;
-    console.error(emsg);
+    log_error(emsg);
     res.status(err.status || 500).send(emsg);
 });
 
-app.listen(3000, () => 
-    console.log("\n===== Ian's Website\nserver running on localhost:3000"));
+
+app.listen(3000, () => {
+    console.log("\n===== Ian's Website");
+    log("server running on http://localhost:3000/");
+});
