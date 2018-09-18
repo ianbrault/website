dcddd2e33677c3c17997a6247513c2aaea8290d9
Ian Brault <ian.brault@engineering.ucla.edu>
2018-09-17 19:19:17 -0700

    Add about page (still needs resume link).

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,15 @@ let About = {
         if (vnode.attrs.active === "about") className += ".container--shown";
         else className += ".container--hidden";
 
-        return m(className);
+        return m(className,
+            m("p.about--title", "about me"),
+            m(".about--content-wrapper",
+                m("img.about--image", {src: "/img/me.jpg"}),
+                m("p.about--copy",
+                    "Hello! My name is Ian, I'm currently a 4th year student at UCLA studying computer science.", m("br"), m("br"), "I mainly work with C and C++, and have recently been messing around with Rust. I also have experience with full-stack web development, Python, and lately some iOS development with React Native. Check out ", m("a.about--link", {href: ""}, "my resume"), " to see more, or check out some of my past & current projects in the nav links above.", m("br"), m("br"), "I'm interested primarily in systems-level programming but am also passionate about programming languages, compilers, and UI/UX design.", m("br"), m("br"), "In my free time, you can find me obsessively following the NBA and UCLA basketball, doing outdoors things, or working on personal projects."
+                ),
+            )
+        );
     }
 };
 
