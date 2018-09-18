9d01060a75b359971185a921e98dd5b785cb6a6f
Ian Brault <ian.brault@engineering.ucla.edu>
2018-09-17 17:42:54 -0700

    Add links to homepage.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,15 @@
 import m from "mithril";
 
 let Home = {
-    view: function() {
+    oninit: function(vnode) {
+        vnode.state.hovered = -1;
+    },
+
+    hover: function(vnode, i) {
+        vnode.state.hovered = i;
+    },
+
+    view: function(vnode) {
         return m(".content.home--container",
             m(".home--image-wrapper",
                 m(".home--back-border"),
@@ -17,6 +25,32 @@ let Home = {
                 m("p.home--title", "ian brault"),
                 m("p.home--subtitle", "junior developer"),
                 m("p.home--subtitle", "UCLA student"),
+                m(".home--icon-container",
+                    m("a.home--icon-wrapper", {
+                        href: "https://github.com/ianbrault",
+                        target: "_blank",
+                        onmouseenter: () => this.hover(vnode, 0),
+                        onmouseleave: () => this.hover(vnode, -1),
+                    }, m("img.home--icon", {
+                        src: `/img/github_${vnode.state.hovered === 0 ? "dark" : "light"}.svg`
+                    })),
+                    m("a.home--icon-wrapper", {
+                        href: "https://www.linkedin.com/in/ianbrault/",
+                        target: "_blank",
+                        onmouseenter: () => this.hover(vnode, 1),
+                        onmouseleave: () => this.hover(vnode, -1),
+                    }, m("img.home--icon", {
+                        src: `/img/linkedin_${vnode.state.hovered === 1 ? "dark" : "light"}.svg`
+                    })),
+                    m("a.home--icon-wrapper", {
+                        href: "https://twitter.com/ianbrault",
+                        target: "_blank",
+                        onmouseenter: () => this.hover(vnode, 2),
+                        onmouseleave: () => this.hover(vnode, -1),
+                    }, m("img.home--icon", {
+                        src: `/img/twitter_${vnode.state.hovered === 2 ? "dark" : "light"}.svg`
+                    })),
+                ),
             ),
         );
     }
