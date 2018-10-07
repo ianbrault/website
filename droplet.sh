26f58d6bd7309f8fd41d1465fcf0933ab6f99f68
Ian Brault <ian.brault@engineering.ucla.edu>
2018-10-06 20:17:52 -0700

    Add SCP command to script

--- droplet.sh
+++ droplet.sh
@@ -1,1 +1,18 @@
-ssh ian@138.68.3.149
+#!/bin/bash
+
+USER=ian
+IPADDR=138.68.3.149
+
+ARGS=("$@")
+N_ARG=1
+
+if [[ $# -gt 0 ]]; then
+    if [[ ${ARGS[0]} = "scp" ]]; then
+        while [[ $N_ARG -lt $# ]]; do
+            scp ${ARGS[$N_ARG]} $USER@$IPADDR:~/website/${ARGS[$N_ARG]}
+            let N_ARG=N_ARG+1
+        done
+    fi
+else
+    ssh $USER@$IPADDR:~/website
+fi
