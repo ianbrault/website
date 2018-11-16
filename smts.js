0aafc4638cd3e93dfe6d3ab56a493939003085eb
Ian Brault <ian.brault@engineering.ucla.edu>
2018-11-15 18:48:45 -0800

    Test message post route

--- smts.js
+++ smts.js
@@ -3,7 +3,26 @@
  * callback URL for the "show me the Skaktis" GroupMe bot
  */
 
+const request = require("request");
+require("dotenv").config();
+
+const msg_post_url = "https://api.groupme.com/v3/bots/post";
+
 exports = module.exports = function(req, res) {
-    console.log(req.body);
+    if (req.body && req.body.text) {
+        let msg = req.body.text.replace(/^\s+|\s+$/g, "").toLowerCase();
+        if (msg === "show me the skaktis") {
+            let post_body = {
+                bot_id: process.env.BOT_ID,
+                text: "SKAKTIS",
+            };
+
+            request.post(msg_post_url, post_body, (err) => {
+                if (err)
+                    console.error(err);
+            });
+        }
+    }
+
     return res.status(200).send("message received");
 }
