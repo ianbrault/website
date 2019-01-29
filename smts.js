2026bfbd5406406929313d1a860d177e3d82d142
Ian Brault <ian.brault@engineering.ucla.edu>
2019-01-29 13:46:37 -0800

    Update skaktis bot for multiple pics.

--- smts.js
+++ smts.js
@@ -8,16 +8,26 @@ require("dotenv").config();
 
 const msg_post_url = "https://api.groupme.com/v3/bots/post";
 
+const images = [
+    "https://i.groupme.com/568x771.png.d702f41e96c14a4bb84651e42f825ec4",
+    "https://i.groupme.com/540x960.png.3e7f3e5cf81d448a8552fccca6e33b0a",
+    "https://i.groupme.com/540x960.png.2f757f0a4aa24ec6b71880dcf8db6dac",
+    "https://i.groupme.com/515x521.png.ded1c63a596d416bb217adbd60deee61",
+];
+
+const getRandom = (max) => Math.floor(Math.random() * Math.floor(max));
+
 exports = module.exports = function(req, res) {
     if (req.body && req.body.text) {
         let msg = req.body.text.replace(/^\s+|\s+$/g, "").toLowerCase();
         if (msg === "show me the skaktis") {
+            let image = images[getRandom(3)];
             let post_body = {
                 json: {
                     bot_id: "0776e81e17083d03af04351603",
                     "attachments" : [{
                         "type": "image",
-                        "url": "https://i.groupme.com/568x771.png.84c4f1dd3f634ef5a44841951137f8a2",
+                        "url": image,
                     }],
                 }
             };
