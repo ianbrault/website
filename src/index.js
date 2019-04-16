cd04e9a94343e9e4814f4d641159cd981cd53160
Ian Brault <ian.brault@engineering.ucla.edu>
2019-04-15 23:26:56 -0700

    Add cursor click effects.

--- src/index.js
+++ src/index.js
@@ -10,3 +10,21 @@ document.addEventListener("mousemove", (event) => {
     cursor.style.top = event.pageY + "px";
     cursor.style.left = event.pageX + "px";
 });
+
+const createClickHalo = (event) => {
+    const halo = document.createElement("div");
+    halo.className = "click-halo";
+    halo.style.top = event.pageY + "px";
+    halo.style.left = event.pageX + "px";
+    return halo;
+};
+
+document.addEventListener("click", (event) => {
+    const halo = createClickHalo(event);
+    document.body.appendChild(halo);
+    setTimeout(() => {
+        const halos = document.getElementsByClassName("click-halo");
+        for (let i = 0; i < halos.length; i++)
+            document.body.removeChild(halos[i]);
+    }, 720);
+})
