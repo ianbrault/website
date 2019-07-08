1eee02548e18949278d4a337ba591f3a985925ca
Ian Brault <ian.brault@engineering.ucla.edu>
2019-07-07 20:52:47 -0700

    Finish SVG header for about page.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
  * author: Ian Brault <ianbrault@ucla.edu>
  */
 
+/* global Snap, mina */
+
 // PAGE TRANSITIONS
 
 const pageTime = 320; // ms
@@ -10,7 +12,7 @@ const navLinks = document.getElementsByClassName("nav-item");
 
 const getSectionFromHref = (href) => {
     let target = href.slice(href.lastIndexOf("#"));
-    if (target === "#")
+    if (target === "#" || target === "/") 
         target = "#home";
 
     return document.querySelector("section" + target);
@@ -19,6 +21,8 @@ const getSectionFromHref = (href) => {
 const updateNavLinks = (href) => {
     for (let i = 0; i < navLinks.length; i++) {
         if (href === navLinks[i].href)
+            navLinks[i].classList.add("nav-item--active");
+        else if (href + "#" === navLinks[i].href)
             navLinks[i].classList.add("nav-item--active");
         else
             navLinks[i].classList.remove("nav-item--active");
@@ -44,6 +48,9 @@ const pageIn = (href) => {
         section.style.display = "flex";
     }, pageTime);
     setTimeout(() => section.classList.remove("page-in"), pageTime * 2);
+
+    if (href.endsWith("#about"))
+        setTimeout(() => onAboutLoad(), pageTime * 1.72);
 };
 
 const navigate = (href) => {
@@ -85,7 +92,6 @@ document.addEventListener("click", (event) => {
 
 // LOGO EFFECTS
 
-/* global Snap */
 const logo = Snap("#logo");
 const wireColor = "#ffd700";
 
@@ -106,3 +112,38 @@ setInterval(() => {
     }, 500);
 }, 800);
 
+// ABOUT PAGE EFFECTS
+
+const aboutHeader = Snap("#about-header");
+const diag = 4 * Math.sin(Math.PI / 4);
+const r = 1.2;
+const rDiag = r * Math.sin(Math.PI / 4);
+
+const animateWire = (polyline, duration, endX, endY) => {
+    Snap.animate(0, 180, (length) => {
+        polyline.attr({ "stroke-dasharray": length + " 200" });
+    }, duration, mina.easeout);
+
+    setTimeout(() => aboutHeader.circle(endX + rDiag, endY + rDiag, r), duration);
+};
+
+const onAboutLoad = () => {
+    const y = [2, 6, 10];
+
+    // wires break downward at 45 degrees
+    // viewBox: 0 0 232 30
+    const wires = [
+        aboutHeader.select("#wire1"),
+        aboutHeader.select("#wire2"),
+        aboutHeader.select("#wire3")
+    ];
+
+    const bp1 = 140, bp2 = bp1 - diag, bp3 = bp2 - diag;
+    wires[0].attr({ points: [0, y[0], bp1, y[0], bp1 + 12, y[0] + 12] });
+    wires[1].attr({ points: [0, y[1], bp2, y[1], bp2 + 12 + rDiag, y[1] + 12 + rDiag, bp2 + 32, y[1] + 12 + rDiag] });
+    wires[2].attr({ points: [0, y[2], bp3, y[2], bp3 + 12, y[2] + 12] });
+
+    animateWire(wires[0], 640, bp1 + 12, y[0] + 12);
+    animateWire(wires[1], 400, bp2 + 32, y[1] + 12);
+    animateWire(wires[2], 520, bp3 + 12, y[2] + 12);
+};
