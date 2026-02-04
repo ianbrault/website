3e79d07431c5ed939670a1eb2b7f0137da9d0c72
Ian Brault <ian@brault.dev>
2026-02-03 22:07:20 -0800

    Fixes to deployment script and docker compose config

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,6 +3,14 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install podman-docker -y
+
+# Install Docker
+sudo apt install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce
 
 docker compose up -d
